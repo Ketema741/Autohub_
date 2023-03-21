17d6d7eadb1578b111606409f4ea650461b8cd60
Ketema Girma <ketemagirma15@gmail.com>
2023-03-21 23:45:45 +0300

    logo fix

--- src/components/SupplierSidebar.jsx
+++ src/components/SupplierSidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { SiShopware } from 'react-icons/si';
+import { GiCartwheel } from 'react-icons/gi';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
@@ -25,7 +25,7 @@ const SupplierSidebar = () => {
         <>
           <div className="flex justify-between items-center">
             <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-              <SiShopware /> <span>Shoppy</span>
+            <GiCartwheel /> <span>AutoHub</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
