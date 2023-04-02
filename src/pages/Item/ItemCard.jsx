407bede2da61af5db45a1fdaabfb23d0d74e8a65
Ketema Girma <kgirma363@gmail.com>
2023-04-02 17:45:30 +0300

    rating icons

--- src/pages/Item/ItemCard.jsx
+++ src/pages/Item/ItemCard.jsx
@@ -1,47 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import { FaStar } from 'react-icons/fa';
+
+
+const RatingIcon = ({ filled }) => {
+  const starColor = filled ? 'text-yellow-300' : 'text-gray-300';
+
+  return <FaStar className={`w-5 h-5 ${starColor}`} />;
+}
 const ItemCard = ({ item, currentColor }) => {
   return (
-    <div tabIndex={0} className="bg-white focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
+    <div className="bg-white focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
       <Link to="item-detail">
         <div>
           <img src={item.itemImages.length >= 1 ? item.itemImages[0].url : '/public/img/gal-1.jpeg'} alt={item.title} className="px-4 pt-4 focus:outline-none w-full h-48" />
         </div>
       </Link>
       <div className="bg-white">
-        <Link to="item-detail">
-
-          <div className="flex items-center justify-between px-4 pt-4">
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" tabIndex={0} className="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
-              </svg>
-            </div>
-            <div className="bg-yellow-200 py-1.5 px-6 rounded-full">
-              <p tabIndex={0} className="focus:outline-none text-xs text-yellow-700">Featured</p>
-            </div>
-          </div>
-        </Link>
         <div className="p-4">
           <Link to="item-detail">
-
-            <div className="flex items-center">
-              <h2 tabIndex={0} className="focus:outline-none text-lg font-semibold">{item.title}</h2>
-              <p tabIndex={0} className="focus:outline-none text-xs text-gray-600 pl-5">4 days ago</p>
+            <div className="flex flex-wrap items-center">
+              <h2 className="text-2xl focus:outline-none font-semibold">
+                {item.title}
+              </h2>
             </div>
           </Link>
+          <div className="flex items-center mt-2.5 mb-5">
+            <RatingIcon filled={true} />
+            <RatingIcon filled={true} />
+            <RatingIcon filled={true} />
+            <RatingIcon filled={false} />
+            <RatingIcon filled={false} />
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3" style={{color:currentColor}}>
+              3.0
+            </span>
+            <p className="focus:outline-none text-xs text-gray-600 pl-2">4 days ago</p>
+          </div>
           <div className="flex mt-4">
-            <div>
-              <p tabIndex={0} className="focus:outline-none text-xs text-gray-600 px-2 bg-gray-200 py-1">12 months warranty</p>
-            </div>
+            <span className="text-xl font-bold text-gray-900 dark:text-white">{item.price} ETB</span>
             <div className="pl-2">
-              <p tabIndex={0} className="focus:outline-none text-xs text-gray-600 px-2 bg-gray-200 py-1">Complete box</p>
+              <p className="text-xs text-gray-600 dark:text-gray-400 px-2 bg-gray-200 py-1 rounded-full">20% discount</p>
             </div>
           </div>
           <div className="flex items-center justify-between py-4">
-            <button tabIndex={0} className="focus:outline-none  text-white font-bold py-2 px-4 rounded" style={{ backgroundColor: currentColor }}>
+            <button className="text-white font-bold py-2 px-4 rounded focus:outline-none" style={{ backgroundColor: currentColor }}>
               Add to cart
             </button>
           </div>
--- src/pages/Supplier/SupplierData.jsx
+++ src/pages/Supplier/SupplierData.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { FaStar } from 'react-icons/fa';
+
 import { GoPrimitiveDot } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
@@ -40,7 +42,14 @@ const DropDown = ({ currentMode }) => (
   </div>
 );
 
+const RatingIcon = ({ filled }) => {
+  const starColor = filled ? 'text-yellow-300' : 'text-gray-300';
+
+  return <FaStar className={`w-5 h-5 ${starColor}`} />;
+}
 const SupplierData = () => {
+
+
   const {
     currentMode,
     currentColor,
@@ -69,48 +78,13 @@ const SupplierData = () => {
                     </h3>
                   </Link>
                   <div className="flex items-center mt-2.5 mb-5">
-                    <svg
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    <svg
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    <svg
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    <svg
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    <svg
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
+                      <RatingIcon filled={true} />
+                      <RatingIcon filled={true} />
+                      <RatingIcon filled={true} />
+                      <RatingIcon filled={false} />
+                      <RatingIcon filled={false} />
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                      5.0
+                      3.0
                     </span>
                   </div>
                   <div className="flex items-center justify-between">
@@ -459,7 +433,7 @@ const SupplierData = () => {
             <img className="md:w-96 h-50 " src={product9} alt="" />
             <div className="mt-8">
               <p className="font-semibold text-lg">React 18 coming soon!</p>
-              <p className="text-gray-400 ">By Johnathan Doe</p>
+              <p className="text-gray-400 ">By Ketema G.</p>
               <p className="mt-8 text-sm text-gray-400">
                 This will be the small description for the news you have shown
                 here. There could be some great info.
