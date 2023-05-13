8025fe585c2a9c562f163d320afff4bf60b98c23
Ketema Girma <kgirma363@gmail.com>
2023-05-13 16:07:09 +0300

    frontend

--- backend/configurations/multer.js
+++
@@ -1,6 +0,0 @@
-const multer = require("multer");
-
-const storage = multer.diskStorage({});
-const upload = multer({ storage });
-
-module.exports = { upload };
--- backend/routes/analyticRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { verifyToken } = require("../middleware/auth");
-const { customerLocation } = require("../controllers/analyticController");
-
-router.get("/customer-by-location", verifyToken, customerLocation);
-
-module.exports = router;
