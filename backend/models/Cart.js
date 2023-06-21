834ed62f575ef4dab359d1f952b8f6256258e6bc
Gatwech Tap Nguth <gatwech3211@gmail.com>
2023-06-21 12:39:32 +0300

    Item is

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -8,7 +8,7 @@ const cartSchema = new mongoose.Schema({
   },
   items: [
     {
-      item: {
+      itemId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Item",
         required: true,
