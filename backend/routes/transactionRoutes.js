e592fe160addf62de1ad8dbcfe382c1661796a82
Gatwech Tap Nguth <gatwech3211@gmail.com>
2023-06-21 23:50:34 +0300

    Querying all transactions

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getTransactions } = require("../configurations/Admin/transactions");
+const { verifyAdministrator } = require("../middleware/auth");
 
 router.get("/get", verifyAdministrator, getTransactions);
 
